0fa62121414418ebd42fda6977b67269c7f9e73b
WalkER14 <Walker.lari123>
2018-06-19 13:09:24 -0400

    Created- exampleData

---
+++ src/example.data.js
@@ -0,0 +1,33 @@
+const exampleData = {
+    title: "example data",
+    logo: "",
+    menuButtons: [
+        {
+            icon:"",
+            text:"Events"
+        },
+        {
+            icon:"",
+            text:"Jobs"
+        },
+        {
+            icon:"",
+            text:"Community"
+        }
+    ],
+    linksSections: [
+        ['About Us','Contact Us']
+        ,
+        ['Comunity','Organizations']
+        ,
+        ['Privacy Police','Cookie Police', 'Data Privacy']
+    ]
+        ,
+    aboutUs: {
+        title: "CO 2018",
+        text: "BlueOceanTech"
+    }
+
+};
+
+export default exampleData;
