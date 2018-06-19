d9bc535a930ee1b1c0e1159a7a4b82f7a3bf7f90
WalkER14 <Walker.lari123>
2018-06-18 23:10:03 -0400

    Created - leftSideMenu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import LeftSideMenu from "./LeftSideMenu/leftSideMenu";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <LeftSideMenu/>
       </div>
     );
   }
---
+++ src/LeftSideMenu/leftSideMenu.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './leftSideMenu.css';
+
+class LeftSideMenu extends Component {
+
+    render() {
+        return (
+            <div className="leftSideMenu">
+
+            </div>
+        );
+    }
+}
+
+export default LeftSideMenu;
