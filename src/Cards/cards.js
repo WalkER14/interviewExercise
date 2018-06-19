c913510a05ff1ee21bba2dea79fc8b3c09991ecd
WalkER14 <Walker.lari123>
2018-06-19 16:01:55 -0400

    Updated- Class Cards

--- src/Cards/cards.js
+++ src/Cards/cards.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import exampleData from '../example.data';
+import exampleData from '../example.data';;
 
 
+const CardTags = (props) =>
+    <div className="tags">
+        <i src={props.tagIcon}/><span>{props.tag}</span>
+    </div>;
 
+const CardContent = (props)=>
+    <div className="cardContent">
+        <CardTags tagIcon={props.tagIcon}/>
+        <h2>{props.header}</h2>
+        <span>{props.sub}</span>
+        <p>{props.text}</p>
+    </div>;
+
+const Card = (props)=>
+    <div className="card">
+        <img alt="" src={require('../assets/images/image1.png')}/>
+        <CardContent tagIcon={props.tagIcon} header={props.header} sub={props.sub} text={props.text}/>
+    </div>;
+
+const CardsContainer  = (props)=>
+    <div className="cardsContainer">
+        {
+            props.cards.map((card,i)=>
+                <Card key={i} header={card.header} img={card.img} sub={card.sub} text={card.text} />
+        )}
+    </div>;
 
 class Cards extends Component {
 
     render() {
         return (
-            <div className="Cards">
-            </div>
+            <CardsContainer cards={exampleData.cards}/>
         );
     }
 }
--- src/example.data.js
+++ src/example.data.js
@@ -1,4 +1,5 @@
 import {_ as Lodash} from 'lodash'
+import image1 from './assets/images/image1.png';
 
 
 const lorem ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -9,7 +10,7 @@ const exampleData = {
     title: "example data",
     logo: "",
     cards:[{
-      img:"",
+      img:'../assets/images/image1.png',
       tagIcon:"",
       tag:"JOB",
       header:"Director of Data Science",
@@ -17,7 +18,7 @@ const exampleData = {
       text: exampleText
     },
     {
-        img:"",
+        img:"../assets/images/image1.png",
         tagIcon:"",
         tag:"JOB",
         header:"Director of Data Science & Analytics",
@@ -25,7 +26,7 @@ const exampleData = {
         text: exampleText
     },
         {
-            img:"",
+            img:"../assets/images/image1.png",
         tagIcon:"",
     tag:"JOB",
     header:"Marketing Director & Interface",
@@ -33,7 +34,7 @@ const exampleData = {
     text: exampleText
 },
         {
-            img:"",
+            img:"../assets/images/image1.png",
             tagIcon:"",
             tag:"JOB",
             header:"Senior Designer UX Experience",
@@ -41,7 +42,7 @@ const exampleData = {
             text: exampleText
         },
         {
-            img:"",
+            img:"../assets/images/image1.png",
             tagIcon:"",
             tag:"JOB",
             header:"Senior Marketing Consultant",
