7965201de0c9282272447e775565b6895b79a3aa
WalkER14 <Walker.lari123>
2018-06-20 14:44:58 -0400

    Updated- cards styles

--- src/Components/Cards/cards.js
+++ src/Components/Cards/cards.js
@@ -5,13 +5,14 @@ import './cards.css'
 
 const CardTags = (props) =>
     <div className="cardTags">
-        <i src={props.tagIcon}/><span>{props.tag}</span>
+        <i src={props.tagIcon} className="material-icons">{props.tagIcon}</i>
+        <span>{props.tag}</span>
     </div>;
 
 const CardContent = (props)=>
     <div className="cardContent">
-        <CardTags tagIcon={props.tagIcon}/>
-        <h3>{props.header}</h3>
+        <CardTags tagIcon={props.tagIcon} tag={props.tag}/>
+        <span className="header">{props.header}</span>
         <span>{props.sub}</span>
         <p>{props.text}</p>
     </div>;
@@ -19,14 +20,14 @@ const CardContent = (props)=>
 const Card = (props)=>
     <div className="card">
         <img alt="" src={props.img}/>
-        <CardContent tagIcon={props.tagIcon} header={props.header} sub={props.sub} text={props.text}/>
+        <CardContent tagIcon={props.tagIcon} tag={props.tag} header={props.header} sub={props.sub} text={props.text}/>
     </div>;
 
 const CardsContainer  = (props)=>
     <div className="cardsContainer">
         {
             props.cards.map((card,i)=>
-                <Card key={i} header={card.header} img={card.img} sub={card.sub} text={card.text} />
+                <Card key={i} header={card.header} tag={card.tag} tagIcon={card.tagIcon} img={card.img} sub={card.sub} text={card.text} />
         )}
     </div>;
 
